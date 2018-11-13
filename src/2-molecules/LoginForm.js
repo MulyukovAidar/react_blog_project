ab8cab7a9a754965634ab2c54a5102b31bd353ae
aidar <rememberlig@gmail.com>
2018-11-13 17:28:47 +0300

    /login component styling fixed

--- src/2-molecules/LoginForm.js
+++ src/2-molecules/LoginForm.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './LoginForm.css'
 import LoginButton from "../1-atoms/LoginButton";
 
-class LoginForm extends React.Component{
-    render(){
-        return(
-            <div className="form-section pure-u-1-3">
-                <form method="post" >
-                    <h1>Введите данные учетной записи</h1>
-                    <input id="username" className="form" name="username"  placeholder="Логин"/>
-                    <input id="password" className="form" name="password" placeholder="Пароль"/>
-                    {/*<button className="" type="submit">Авторизоваться</button>*/}
-                    <LoginButton/>
+class LoginForm extends React.Component {
+    render() {
+        return (
+          <div className="wide">
+                <div className="form-section pure-u-1-3">
+                    <form method="post">
+                        <h1>Введите данные учетной записи</h1>
+                        <input id="email" className="form" name="email" placeholder="example@email.com"/>
+                        <input id="password" className="form" name="password" placeholder="*******"/>
+                        <LoginButton/>
                     </form>
-            </div>
+                </div>
+          </div>
         );
     }
 }
