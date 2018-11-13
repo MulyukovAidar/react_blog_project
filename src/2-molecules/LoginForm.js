ef863accd5204d02aad3770af188abd7cc8fccb6
aidar <rememberlig@gmail.com>
2018-11-13 19:40:17 +0300

    added registration link and routing

--- src/2-molecules/LoginForm.js
+++ src/2-molecules/LoginForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './LoginForm.css'
 import LoginButton from "../1-atoms/LoginButton";
+import RegisterLink from '../1-atoms/RegisterLink'
 
 class LoginForm extends React.Component {
     render() {
         return (
           <div className="wide">
+                {/* todo fix width on mobile devices*/}
                 <div className="form-section pure-u-1-3">
                     <form method="post">
                         <h1>Введите данные учетной записи</h1>
@@ -13,6 +15,7 @@ class LoginForm extends React.Component {
                         <input id="password" className="form" name="password" placeholder="*******"/>
                         <LoginButton/>
                         <hr className="line"/>
+                        <RegisterLink/>
                     </form>
                 </div>
           </div>
