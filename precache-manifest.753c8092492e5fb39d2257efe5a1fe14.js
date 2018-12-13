2ee93d4482f07d6807711e3e4294a685808ff21c
aidar <rememberlig@gmail.com>
2018-12-13 16:21:56 +0300

    Updates

---
+++ precache-manifest.753c8092492e5fb39d2257efe5a1fe14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b919cadb410c7ab5b1a494e9bcea8b67",
+    "url": "/react_blog_project/static/media/logo.b919cadb.jpg"
+  },
+  {
+    "revision": "fa2bdb09f39c100b526a",
+    "url": "/react_blog_project/static/js/runtime~main.fa2bdb09.js"
+  },
+  {
+    "revision": "a2d2f519a480d5cb3810",
+    "url": "/react_blog_project/static/js/main.a2d2f519.chunk.js"
+  },
+  {
+    "revision": "cfa5afc6a593f3890f89",
+    "url": "/react_blog_project/static/js/1.cfa5afc6.chunk.js"
+  },
+  {
+    "revision": "a2d2f519a480d5cb3810",
+    "url": "/react_blog_project/static/css/main.e19d744e.chunk.css"
+  },
+  {
+    "revision": "cfa5afc6a593f3890f89",
+    "url": "/react_blog_project/static/css/1.320308ab.chunk.css"
+  },
+  {
+    "revision": "8747c329cdb0c85782aca6c308d986b3",
+    "url": "/react_blog_project/index.html"
+  }
+];
